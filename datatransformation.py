a52598b5208f0cd22e115484d1edbb8835cd3c6f
James Sullivan <sullivan.a.james@hotmail.com>
2021-04-11 17:05:19 +0100

    Added some explantory text to top of script

--- datatransformation.py
+++ datatransformation.py
@@ -1,12 +1,16 @@
+'''
+Script containing convenient data transofrmation functions
+'''
 
-#Data Transposition Function: 
+
+#Data Transposition Function:
 
 def transpose(dataframe,values,keys=None):
-    
+
     """
-    The transpose function allows you to 'flip' the orientation of a pandas dataframe whereby 
+    The transpose function allows you to 'flip' the orientation of a pandas dataframe whereby
     one or many horizontal data columns can be viewed on a vertical axis
-    
+
     Parameters
     ----------
     dataframe : pandas dataframe
@@ -15,21 +19,21 @@ def transpose(dataframe,values,keys=None):
         Select the column name or list of column names which will be pivoted so as that
         their horizontally stored data is re-positioned on a vertical axis.
     keys : string or list
-        Select the column name or list of column names to pivot the dataframe around. 
-        The name of these column(s) will remain on the horizontal axis, 
+        Select the column name or list of column names to pivot the dataframe around.
+        The name of these column(s) will remain on the horizontal axis,
         with values replicated vertically for each column selected as values.
 
     Returns
     -------
     DataFrame
-        The number of rows your dataframe will end up with after transposing equates to 
+        The number of rows your dataframe will end up with after transposing equates to
         the number of initial rows times-by the number of values columns selected.
-    
+
     """
     import pandas as pd
-    
+
     null_index_name = 0
-    
+
     if dataframe.index.name is None:
         null_index_name += 1
         i = 0
@@ -37,15 +41,15 @@ def transpose(dataframe,values,keys=None):
         while dataframe.index.name in list(dataframe.columns.values):
             i += 1
             dataframe.index.name = str(i)
-    
+
     if type(values) != list:
         values = [values]
-        
+
     if keys is None:
         keys = []
     elif type(keys) != list:
         keys = [keys]
-    
+
     AttributeName = "AttributeName"
     AttributeValue = "AttributeValue"
 
